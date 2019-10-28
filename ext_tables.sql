f4c4a5226d998b80e4ae655384cb18e3410fa8d5
Wolfgang Klinger <wk@plan2.net>
2019-10-28 07:47:37 +0100

    [TASK] Adapt the sys_redirect table definitions

--- ext_tables.sql
+++ ext_tables.sql
@@ -13,6 +13,6 @@ CREATE TABLE pages (
 #
 CREATE TABLE sys_redirect (
   source_host varchar(255) DEFAULT '' NOT NULL,
-  source_path TEXT NOT NULL,
+  source_path varchar(2048) DEFAULT '' NOT NULL,
   KEY index_source (source_host(80),source_path(80))
 );
