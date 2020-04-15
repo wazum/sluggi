f168aef782b7e61a3606cb8bf82f01b4ceca7423
Wolfgang Klinger <wk@plan2.net>
2020-04-15 11:33:24 +0200

    [BUGFIX] Check for special pages (doktype) that get no slug by default

--- Classes/Helper/SlugHelper.php
+++ Classes/Helper/SlugHelper.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Wazum\Sluggi\Helper;
 
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -24,23 +25,31 @@ class SlugHelper
      */
     public static function getSlug(int $pageId, int $languageId = 0): string
     {
+        $rootLine = BackendUtility::BEgetRootLine($pageId, '', true, ['nav_title']);
+        do {
+            $pageRecord = array_shift($rootLine);
+            // Exclude spacers, recyclers, folders and everything else which has no slug
+        } while (!empty($rootLine) && (int)$pageRecord['doktype'] >= 199);
+
         /** @var QueryBuilder $queryBuilder */
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
             ->getQueryBuilderForTable('pages');
 
         if ($languageId > 0) {
-            return (string)$queryBuilder->select('slug')
+            $slug = (string)$queryBuilder->select('slug')
                 ->from('pages')
                 ->where(
-                    $queryBuilder->expr()->eq('l10n_parent', $pageId),
+                    $queryBuilder->expr()->eq('l10n_parent', $pageRecord['uid']),
                     $queryBuilder->expr()->eq('sys_language_uid', $languageId)
+                )->execute()->fetchColumn();
+        } else {
+            $slug = (string)$queryBuilder->select('slug')
+                ->from('pages')
+                ->where(
+                    $queryBuilder->expr()->in('uid', $pageRecord['uid'])
                 )->execute()->fetchColumn();
         }
 
-        return (string)$queryBuilder->select('slug')
-            ->from('pages')
-            ->where(
-                $queryBuilder->expr()->in('uid', $pageId)
-            )->execute()->fetchColumn();
+        return $slug === '/' ? '' : $slug;
     }
 }
