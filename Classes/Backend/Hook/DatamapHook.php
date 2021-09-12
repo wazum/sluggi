ca4924e3ee8e04ee833212e189580f60dee418e0
Wolfgang Klinger <wk@plan2.net>
2021-09-12 11:22:15 +0200

    [BUGFIX] Use only the current slug segment if user has no permissions for full slug update
    Fixes #64

--- Classes/Backend/Hook/DatamapHook.php
+++ Classes/Backend/Hook/DatamapHook.php
@@ -7,7 +7,10 @@ namespace Wazum\Sluggi\Backend\Hook;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use Wazum\Sluggi\Helper\Configuration;
+use Wazum\Sluggi\Helper\PermissionHelper;
 use Wazum\Sluggi\Helper\SlugHelper;
+use function rtrim;
 
 /**
  * Class DatamapHook
@@ -71,10 +74,16 @@ class DatamapHook
     {
         $currentPage = BackendUtility::getRecord('pages', $id, 'uid, slug, sys_language_uid');
         if (!empty($currentPage)) {
-            $languageId = $currentPage['sys_language_uid'];
+            $allowOnlyLastSegment = (bool)Configuration::get('last_segment_only');
+
             $currentSlugSegment = SlugHelper::getLastSlugSegment($currentPage['slug']);
-            $parentSlug = SlugHelper::getSlug($targetId, $languageId);
-            $newSlug = rtrim($parentSlug, '/') . $currentSlugSegment;
+            if ($allowOnlyLastSegment && !PermissionHelper::hasFullPermission()) {
+                $newSlug = $currentSlugSegment;
+            } else {
+                $languageId = $currentPage['sys_language_uid'];
+                $parentSlug = SlugHelper::getSlug($targetId, $languageId);
+                $newSlug = rtrim($parentSlug, '/') . $currentSlugSegment;
+            }
 
             $data = [];
             $data['pages'][$id]['slug'] = $newSlug;
